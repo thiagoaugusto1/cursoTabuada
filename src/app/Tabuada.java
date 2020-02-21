2d8414b364a14a1396c1114bbf077231eb06cb1d
Thiago Augusto <thiago.aug94@gmail.com>
2020-02-21 08:50:08 -0300

    apagando espaços desnecessarios

--- src/app/Tabuada.java
+++ src/app/Tabuada.java
@@ -19,28 +19,7 @@ public class Tabuada {
 		System.out.println("----------------------------------------------------------------------------------");
 		System.out.println();
 	
-		
-		
-		int mult = 11;
-		int tab = 11;
-		int[][] resultado = new int[mult][tab];
-
-		for (int c = 1; c < mult; c++) {
-		    for (int a = 1; a < tab; a++) {
-		        int result = c * a;
-		        resultado[c][a] = result;
-		        System.out.println(c + "x" + a + "=" + resultado[c][a]);
-		       
-		    }
-		    System.out.println();
-		}
-
-		System.out.println("----------------------------------------------------------------------------------");
-		System.out.println();
-		
 	
-
-		
 	}
 			
 	}	
